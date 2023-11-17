cbc75972fecc54d9f962b9e2df7e111ef5dfa336
Syed Nouman <noumanafzal4@gmail.com>
2023-11-17 12:37:39 +0530

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -154,7 +154,7 @@ const Navbar = () => {
           onClick={() => handleScroll('.new-arrival')}
         >
           {' '}
-          <Link to="/">new arrival</Link>
+          <Link to="/">Creators</Link>
         </Item>
       </MenuItems>
     </NavContainer>
